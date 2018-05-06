dbfc064fd4ab0785bf4d44c428f8c58e7410174b
jsarroyo <jsarroyo@github.com>
2018-05-06 17:03:18 -0600

    Conexion por postgress sql
    Conexion por postgress sql

--- GitClasesGenericas/Connection_QueryPsg.cs
+++ GitClasesGenericas/Connection_QueryPsg.cs
@@ -1,54 +1,48 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GitClasesGenericas
 {
-    public class Connection_Query
+    public class Connection_QueryPsg
     {
-
-        string ConnectionString = "";
-        SqlConnection con;
+        private string ConnectionString;
+        NpgsqlConnection con;
 
         public void OpenConection()
         {
-            con = new SqlConnection(ConnectionString);
+            con = new NpgsqlConnection(ConnectionString);
             con.Open();
         }
-
-
         public void CloseConnection()
         {
             con.Close();
         }
 
-
         public void ExecuteQueries(string Query_)
         {
-            SqlCommand cmd = new SqlCommand(Query_, con);
+            NpgsqlCommand cmd = new NpgsqlCommand(Query_, con);
             cmd.ExecuteNonQuery();
         }
-
-
-        public SqlDataReader DataReader(string Query_)
+        public NpgsqlDataReader  DataReader(string Query_)
         {
-            SqlCommand cmd = new SqlCommand(Query_, con);
-            SqlDataReader dr = cmd.ExecuteReader();
+            NpgsqlCommand cmd = new NpgsqlCommand(Query_, con);
+            NpgsqlDataReader dr = cmd.ExecuteReader();
             return dr;
         }
-
-
         public object ShowDataInGridView(string Query_)
         {
-            SqlDataAdapter dr = new SqlDataAdapter(Query_, ConnectionString);
+            NpgsqlDataAdapter  dr = new NpgsqlDataAdapter(Query_, ConnectionString);
             DataSet ds = new DataSet();
             dr.Fill(ds);
             object dataum = ds.Tables[0];
+
             return dataum;
         }
+         
     }
 }
